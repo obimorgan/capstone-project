4efa2e19090f5fc3a43efe72a115915f88fab5ba
Roby Morgan <roby.c.morgan@gmail.com>
2022-03-01 17:23:34 +0100

    login layout.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import InputAdornment from '@mui/material/InputAdornment'
+import TextField from '@mui/material/TextField/TextField'
+import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button'
+import { Mail } from '@mui/icons-material'
+
+import { useState } from 'react'
 
 export default function Login() {
+    const [email, setEmail] = useState("")
     return (
-        <div>Login</div>
+
+        <Box>
+            <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center ', height: '100vh', justifyContent: 'center' }}>
+                <Box >
+                    <TextField
+                        // label="With normal TextField"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '27ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start"><Mail /></InputAdornment>,
+                        }}
+                    />
+                    <TextField
+                        // label="With normal TextField"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '27ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start"><LockIcon /></InputAdornment>,
+                        }}
+                    />
+                    <Button sx={{ m: 1, width: '35ch' }} variant="contained" color="success">Sign Up</Button>
+                </Box>
+            </Container >
+        </Box>
     )
 }
--- client/src/redux/actions.tsx
+++ client/src/redux/actions.tsx
@@ -5,3 +5,14 @@ export const ACTIONS = {
     USER_LOGIN: 'USER_LOGIN',
     ADD_USER_TO_GAME_ROOM: 'ADD_USER_TO_GAME_ROOM'
 }
+
+export const userLoginAction = () => {
+    return { type: ACTIONS.USER_LOGIN } 
+}
+
+export const addUserToGameroomAction = (user: IUser) => {
+    return {
+        type: ACTIONS.ADD_USER_TO_GAME_ROOM,
+        payload: user
+    }
+}
