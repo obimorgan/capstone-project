3b77a8d13d93c6804969ef0d3c775a7ce5d2d2f1
Roby Morgan <roby.c.morgan@gmail.com>
2022-03-01 13:48:59 +0100

    inital set-up.

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
+import Login from './components/Login';
+import Registration from './components/Registration';
+import Profile from './components/Profile';
+import Watitingroom from './components/Watitingroom';
+import Scoreboard from './components/Scoreboard';
+import Gameroom from './components/Gameroom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/waitingroom" element={<Watitingroom />} />
+        <Route path="/scoreboard" element={<Scoreboard />} />
+        <Route path="/gameroom" element={<Gameroom/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ client/src/components/Watitingroom.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Watitingroom() {
+    return (
+        <div>Watitingroom</div>
+    )
+}
