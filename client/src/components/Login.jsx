5696ab28f534e3128bb0ededc7ddde1937061862
Roby Morgan <roby.c.morgan@gmail.com>
2022-03-02 11:03:21 +0100

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,40 +1,57 @@
-import Container from '@mui/material/Container'
-import Grid from '@mui/material/Grid'
+import { Mail } from '@mui/icons-material'
+import LockIcon from '@mui/icons-material/Lock'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
 import InputAdornment from '@mui/material/InputAdornment'
 import TextField from '@mui/material/TextField/TextField'
-import LockIcon from '@mui/icons-material/Lock';
-import Button from '@mui/material/Button'
-import { Mail } from '@mui/icons-material'
-
+import Badge from '@mui/material/Badge'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GoogleIcon from '@mui/icons-material/Google';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
 import { useState } from 'react'
+
 
 export default function Login() {
     const [email, setEmail] = useState("")
     return (
-
-        <Box>
-            <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center ', height: '100vh', justifyContent: 'center' }}>
-                <Box >
-                    <TextField
-                        // label="With normal TextField"
-                        id="outlined-start-adornment"
-                        sx={{ m: 1, width: '27ch' }}
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start"><Mail /></InputAdornment>,
-                        }}
-                    />
-                    <TextField
-                        // label="With normal TextField"
-                        id="outlined-start-adornment"
-                        sx={{ m: 1, width: '27ch' }}
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start"><LockIcon /></InputAdornment>,
-                        }}
-                    />
-                    <Button sx={{ m: 1, width: '35ch' }} variant="contained" color="success">Sign Up</Button>
-                </Box>
-            </Container >
-        </Box>
+        <Container sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center ', height: '100vh', justifyContent: 'center' }}>
+            <Box >
+                <TextField
+                    id="outlined-start-adornment"
+                    sx={{ m: 1, width: '27ch' }}
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start"><Mail /></InputAdornment>,
+                    }}
+                />
+                <TextField
+                    id="outlined-start-adornment"
+                    sx={{ m: 1, width: '27ch' }}
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start"><LockIcon /></InputAdornment>,
+                    }}
+                />
+                <Button sx={{ m: 1, width: '35ch' }} variant="contained" color="success">Sign Up</Button>
+                <Grid sx={{ display: 'flex' }}>
+                    <IconButton size="large">
+                        <Badge>
+                            <FacebookIcon />
+                        </Badge>
+                    </IconButton>
+                    <IconButton size="large">
+                        <Badge>
+                            <GoogleIcon />
+                        </Badge>
+                    </IconButton>
+                    <IconButton size="large">
+                        <Badge>
+                            <InstagramIcon />
+                        </Badge>
+                    </IconButton>
+                </Grid>
+            </Box>
+        </Container >
     )
 }
